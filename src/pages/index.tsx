590d15c81f54334c9997c1c3b3091da1d478f0c4
Gustavo Ribeiro <git@gustavoribeiro.com>
2021-05-13 10:27:51 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from 'styles/Home.module.css'
 
-function Home() {
+const Home: React.FC = () => {
   return (
     <div className={styles.container}>
       <Head>
